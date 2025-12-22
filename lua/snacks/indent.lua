f8d22b1bf69652b42d872ec06107ca8409dd6f60
rodvicj <rodvicj@gmail.com>
2025-12-22 13:58:40 +0800

    thin

--- lua/snacks/indent.lua
+++ lua/snacks/indent.lua
@@ -51,7 +51,7 @@ local defaults = {
   scope = {
     enabled = true, -- enable highlighting the current scope
     priority = 200,
-    char = "│",
+    char = "▏",
     underline = false, -- underline the start of the scope
     only_current = false, -- only show scope in the current window
     hl = "SnacksIndentScope", ---@type string|string[] hl group for scopes
@@ -70,7 +70,8 @@ local defaults = {
       -- corner_top = "╭",
       -- corner_bottom = "╰",
       horizontal = "─",
-      vertical = "│",
+      -- vertical = "│",
+      vertical = "▏",
       arrow = ">",
     },
   },
